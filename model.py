9f631556638470d2b4cca86b14b7b659309cfe16
Arischang <awoo.jiaming@gmail.com>
2019-11-11 16:49:55 +0800

    [Add] get_data flow.

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import numpy as np
 
-def dense(x, latent_dim, hidden_dim, num, name = 'dense'):
+def dense(x, latent_dim, hidden_dim, num = 5, name = 'dense'):
     with tf.variable_scope(name):
         for i in range(num):
             x = tf.layers.dense(x, hidden_dim, activation = 'relu', name = 'dense_' + str(i))
@@ -10,31 +10,27 @@ def dense(x, latent_dim, hidden_dim, num, name = 'dense'):
 
 def split(x):
     with tf.name_scope('split'):
-        dim = self.x.shape()[-1]
-        x = tf.reshape(self.x, [-1, dim / 2, 2])
+        hidden_dim = int(x.shape[-1] / 2)
+        x = tf.reshape(x, [-1, hidden_dim, 2])
     return x[:,:,0], x[:,:,1]
 
 def concat(x):
     with tf.name_scope('concat'):
-        x = tf.concat(x, axis = 2)
-        x = tf.reshape(x, np.prod(x.shape[1:]))
+        x = tf.concat(x, axis = 1)
     return x
 
 class baseCoupleLayer:
-    def __init__(self, x, dim):
-        self.x = x
-        self.hidden_dim = dim
-        self.latent_dim = int(dim / 2)
-
-    def forward(self):
-        x1, x2 = split(self.x)
-        x2 = self.coupling(x2, dense(x1, self.latent_dim, self.hidden_dim, name = 'forward'), inverse = False)
+    def forward(self, x, hidden_dim, name = 'forward'):
+        latent_dim = int(hidden_dim / 2)
+        x1, x2 = split(x)
+        x2 = self.coupling(x2, dense(x1, latent_dim, hidden_dim, name = name), inverse = False)
         x = concat([x1, x2])
         return x
     
-    def inverse(self):
+    def inverse(self, x, hidden_dim, name = 'inverse'):
+        latent_dim = int(hidden_dim / 2)
         x1, x2 = split(self.x)
-        x2 = self.coupling(x2, dense(x1, self.latent_dim, self.hidden_dim, name = 'inverse'), inverse = True)
+        x2 = self.coupling(x2, dense(x1, latent_dim, hidden_dim, name = name), inverse = True)
         x = concat([x1, x2])
         return x
     
@@ -57,6 +53,8 @@ class multiplicativeCoupleLayer(baseCoupleLayer):
 
 class NICE():
     def __init__(self, input_dim):
+        self.input_dim = input_dim
+
         self.layer_1 = additiveCoupleLayer()
         self.layer_2 = additiveCoupleLayer() 
         self.layer_3 = additiveCoupleLayer()
@@ -64,45 +62,41 @@ class NICE():
         self.diag = tf.Variable(tf.ones(input_dim), name = 'diag')
 
     def encoder(self, x):
-        x = self.layer_1.forward(x)
-        x = self.layer_2.forward(x)
-        x = self.layer_3.forward(x)
-        x = self.layer_4.forward(x)
+        x = self.layer_1.forward(x, self.input_dim, name = 'en_f_1')
+        x = self.layer_2.forward(x, self.input_dim, name = 'en_f_2')
+        x = self.layer_3.forward(x, self.input_dim, name = 'en_f_3')
+        x = self.layer_4.forward(x, self.input_dim, name = 'en_f_4')
         x = tf.matmul(x, tf.diag(tf.exp(self.diag)))
 
         return x
 
     def decoder(self, x):
         x = tf.matmul(x, tf.diag(tf.reciprocal(tf.exp(self.diag))))
-        x = self.layer_4.inverse(x)
-        x = self.layer_3.inverse(x)
-        x = self.layer_2.inverse(x)
-        x = self.layer_1.inverse(x)
+        x = self.layer_4.inverse(x, self.input_dim, name = 'en_b_1')
+        x = self.layer_3.inverse(x, self.input_dim, name = 'en_b_1')
+        x = self.layer_2.inverse(x, self.input_dim, name = 'en_b_1')
+        x = self.layer_1.inverse(x, self.input_dim, name = 'en_b_1')
 
         return x
 
-def model(x, y, dim = 784):
-    with tf.name_scope('input'):
-        X = tf.placeholder(tf.float32, [None, dim], name='image')
-        Y = tf.placeholder(tf.int32, [None], name='label')
-
-    model = NICE(input_dim)
+def model(sess, train_iterator, test_iterator, dim = 784, reuse = False):
+    def m_loss(x, diag, labels = 10):
+        with tf.variable_scope('model', reuse = reuse):
+            loss = tf.reduce_mean(-(tf.reduce_sum(diag) \
+                - tf.reduce_sum(tf.math.log1p(tf.exp(x)) + tf.math.log1p(tf.exp(-x)), axis = 1)))
+        return loss
+    _x, _y = train_iterator
+    model = NICE(_x.shape[-1])
     with tf.variable_scope('encoder', reuse = reuse):
-        x = model.encoder(X)
-
-    en_loss = loss(x, model.diag)
+        x = model.encoder(_x)
     
-    en_var = [var for var in tf.trainable_variables() if 'encoder' in var.name]
-    with tf.name_Scope('en_optmize'):
-        en_optm = tf.train.AdamOptimizer(0.0001, beta1 = 0.0, beta2 = 0.9).minimize(en_loss, var_list = en_var)
+    loss = m_loss(x, model.diag)
+    
+    var = tf.trainable_variables()
+    with tf.name_scope('optimizer'):
+        optimizer = tf.train.AdamOptimizer(0.0001, beta1 = 0.0, beta2 = 0.9).minimize(loss, var_list = var)
 
-
-
-def loss(x, diag, labels = 10):
-    with tf.variable_scope('model', reuse = reuse):
-        loss = tf.reduce_mean(-(tf.reduce_sum(diag) \
-            - tf.reduce_sum(tf.math.log1p(tf.exp(x)) + tf.math.log1p(tf.exp(-x)), axis = 1)))
-    return loss
+    # train = sess.run([optimizer, en_loss])
 
     
 
--- train.py
+++ train.py
@@ -1,24 +1,36 @@
 import tensorflow as tf
 import numpy as np
-
-def getData():
+tf.compat.v1.disable_eager_execution()
+def get_data(n_batch_train = 32, n_batch_test = 32, size = 28):
+    def make_iterator(flow):
+        x, y = flow.next()
+        x = tf.reshape(x, [-1, size * size])
+        y = tf.reshape(y, [-1])
+        return x, y
+   
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    img_size = x_train.shape[1] # 28
-    x_train = np.reshape(x_train, [-1, img_size * img_size])
-    x_test = np.reshape(x_test, [-1, img_size * img_size])
-    x_train, x_test = x_train / 255.0, x_test / 255.0
+    x_train = np.reshape(x_train, [-1, size, size, 1])
+    x_test = np.reshape(x_test, [-1, size, size, 1])
 
-    x, y = (x_train, y_train), (x_test, y_test)
-    
-    return x, y
+    ImageDataGenerator = tf.keras.preprocessing.image.ImageDataGenerator
+
+    train_generator = ImageDataGenerator(rescale = 1./255, width_shift_range = 0.1, height_shift_range = 0.1)
+    test_generator = ImageDataGenerator(rescale = 1./255)
+
+    train_generator.fit(x_train)
+    train_iterator = make_iterator(train_generator.flow(x_train, y_train, n_batch_train))
+
+    test_generator.fit(x_test)
+    test_iterator = make_iterator(test_generator.flow(x_test, y_test, n_batch_test, shuffle = False))
+
+    return train_iterator, test_iterator 
 
 def main():
-    
-    train, test = getData()
-   
-    sess = tf.compat.v1.Session()
-    model = model.model(sess, train, 784)
+    train, test = get_data()
+    with tf.compat.v1.Session() as sess:
+        import model
+        model = model.model(sess, train, test)
 
-
+main()
